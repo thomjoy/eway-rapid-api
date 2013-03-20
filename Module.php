615e9b34186cb5e6bd62cb372d3b4e6a2ebdb070
Tom Joy <tom@4mation.com>
2013-03-20 15:43:24 +1100

    more helpful error

--- Module.php
+++ Module.php
@@ -5,7 +5,17 @@ class Module
 {
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        $configFile = __DIR__ . '/config/module.config.php';
+        
+        if( file_exists($configFile) )
+        {
+            return include_once($configFile);
+        }
+        else
+        {
+            die("Please fill in eWay API credentials in " . __DIR__ . "/config/module.config.dist.php.<br />
+                Once done, rename file to <strong>module.config.php</strong> and hit refresh.");
+        }
     }
 
     public function getAutoloaderConfig()
